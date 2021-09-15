45ea071f975cd6d120e503410ab1427450041741
Nzenwa900 <cyrilnze@gmail.com>
2021-09-15 08:16:33 +0100

    added the release

--- team.js
+++ team.js
@@ -1,1 +1,10 @@
 // create a function that returns the value of of cars//
+/* release zero
+    create a function that returns a value of cars(three cars)
+
+toyota = 30k, benz = 40k, peugeot = 25k, audi = 38k, BWM = 35K, Landrover = 60k
+ the threee most expensive combination of cars
+ */
+
+ 
+    
